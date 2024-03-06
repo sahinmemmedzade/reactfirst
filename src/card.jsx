ba65703e17c21baf8fe8ea3de31a2ac555519782
sahinmemmedzade <shahinim@code.edu.az>
2024-03-06 11:00:38 +0400

    add

---
+++ src/card.jsx
@@ -0,0 +1,10 @@
+
+import Info from "./info";
+const Card=()=>{
+    return(
+        <div className="card">
+            <Info></Info>
+        </div>
+    )
+}
+export default Card
---
+++ src/info.jsx
@@ -0,0 +1,12 @@
+
+const Info=()=>{
+    return(
+        <div className="Info">
+            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS9QavNtGS7sJgU6GDW0d5Q4y6ayl5ORF4FQGQu99z5dg&s" alt="payiz" />
+            <h2>keyfiyetli</h2>
+            <p>24$</p>
+        </div>
+
+    )
+}
+export default Info;
